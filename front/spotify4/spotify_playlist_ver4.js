020cb97d72fcd8a85fd68eb2508e26dd779b5552
MY_PC <sysking1009@naver.com>
2026-02-03 01:12:30 +0900

    2월 2일~3일까지 spotify 플리 추천 화면 거의 완임 위에 버튼만 만들면 거의 끝날듯?

---
+++ front/spotify4/spotify_playlist_ver4.js
@@ -0,0 +1,45 @@
+const iframes= document.querySelectorAll("iframe[data-src]");
+
+const observer = new IntersectionObserver(
+    (entries, obs) => {
+        entries.forEach(entry => {   
+            if (entry.isIntersecting){  // ✅ 올바른 조건
+            const iframe = entry.target;
+
+            // 아직 로드 안되었다면
+            if (!iframe.src) {
+                iframe.src = iframe.dataset.src;
+            }
+            // 다시 관찰하지 않음
+            obs.unobserve(iframe);
+        }
+        });
+    },
+    {
+        threshold: 1.0 // iframe이 100% 보일 때만 로드
+    }
+);
+// 모든 iframe 관찰 시작
+iframes.forEach(iframe => observer.observe(iframe));
+
+
+// ✅ 좌측 가수 목록 클릭 → 우측 유사가수만 보여주는 로직
+const leftLinks = document.querySelectorAll(".artist-link-block");
+const rightBlocks = document.querySelectorAll(".dropbtn");
+
+leftLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    const artistId = link.dataset.artist;
+
+    // 모든 유사가수 블럭 숨김 및 강조 제거
+    rightBlocks.forEach(block => {
+      block.classList.remove("active", "highlight");
+    });
+
+    // 해당 가수 블럭만 보이도록 active + highlight 추가
+    const targetBlock = document.querySelector(`.dropbtn[data-artist="${artistId}"]`);
+    if (targetBlock) {
+      targetBlock.classList.add("active", "highlight");
+    }
+  });
+});
